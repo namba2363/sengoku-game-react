98f9a006e423703f4e8488f63219ca1dea856613
unknown <hn2363ybky0123@gmail.com>
2023-12-17 10:51:30 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(process.env.REACT_APP_URL, process.env.REACT_APP_ANON);
 
 function App() {
+  const [samurai, setSamurai] = useState([]);
+
+  useEffect(() => {
+    getSamurai();
+  }, []);
+
+  async function getSamurai() {
+    const { data } = await supabase.from("samurai").select();
+    setSamurai(data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {samurai.map((samurai) => (
+        <li key={samurai.name}>{samurai.name}</li>
+      ))}
+    </ul>
   );
 }
 
